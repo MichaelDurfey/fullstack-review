0ccffb2ee0c3c74eff6d55aae48437442bfd71c0
Michael Durfey <mdurfey@me.com>
2018-02-17 11:41:27 -0800

    configuring mongoose for deployment

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 
